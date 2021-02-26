c7e13fb3fb2316af2d2e9e85ad85de08d85a0979
Peter Kos <pkos91@icloud.com>
2021-02-25 23:53:37 -0500

    Fix emoji in onboarding messages

--- messages.py
+++ messages.py
@@ -23,12 +23,12 @@ def user_setup(name: str, auth_url: str) -> str:
 
 
 PENDING = inspect.cleandoc("""Uh oh, it looks like you haven't received an acceptance to WiCHacks.
-        If you do receive an acceptance just react with a :bricks: in WiCHacks's Discord server and I can get you set up!""")
+        If you do receive an acceptance just click the 👩‍💻 in WiCHacks's Discord server again, and I can get you set up!""")
 
 ACCEPTED = RSVP_CONFIRMED = inspect.cleandoc("Awesome, you're all set to join! I've given you access to the channels and reformatted your name to match the event requirements.")
 
 WAITLIST = LATE_WAITLIST = inspect.cleandoc("""Hang tight, it looks like you haven't received an acceptance to WiCHacks and you're on the waitlist.
-        If you do receive an acceptance just react with a :bricks: in WiCHacks's Discord server and I can get you set up!""")
+        If you do receive an acceptance just click the 👩‍💻 in WiCHacks's Discord server again, and I can get you set up!""")
 
 DENIED = inspect.cleandoc("""Uh oh, it looks like you haven't received an acceptance to WiCHacks. We were overjoyed with the number of applicants we received, but unfortunately we can not accept everyone due to capacity.
         We invite you to apply again next year. There are plenty of other hackathons this season, and it may not be too late to apply for those. Checkout https://mlh.io to find out more information.""")
@@ -37,7 +37,7 @@ RSVP_DENIED = inspect.cleandoc("""Uh oh, it looks like you told us you weren't c
         You're all set to join! I've given you access to the channels and reformatted your name to match the event requirements.""")
 
 MISSING_INFORMATION = inspect.cleandoc(f"""Uh oh, it looks like you account is missing some information! Go here to the following link to fix the problem: {config.APPLY_URL}.
-        Once done, come back and react with a :bricks: in WiCHacks's Discord server and I can get you set up!""")
+        Once done, come back and click the 👩‍💻 in WiCHacks's Discord server again, and I can get you set up!""")
 
 
 ACC_STATUS_MESSAGES: Dict[str, str] = {
